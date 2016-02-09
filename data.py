c21dce6cb06b92c65059968e8086fa55beefe334
ubuntumax <antrikohs@gmail.com>
2016-02-09 00:55:51 +0000

    Added more comments, readme and license.

--- data.py
+++ data.py
@@ -6,54 +6,77 @@ import numpy as np
 from keras.utils import np_utils
 
 
-def extract_data():
+def extract_data(size=256):
+    '''Extracts the data from the downloaded_images folders
+        Attributes:
+            size: The size to which to resize the images. All images must be the same size so that
+            they can be trained using a Deep CNN.
+            e.g size=256, images will be 256x256
+        Returns a list(X) with the images and their labels(y) in string form, e.g.('dog')
+    '''
+
     X = []
     y = []
     dir = os.path.dirname(os.path.abspath(__file__))
 
     for subdir, dir, files in os.walk(dir+"/downloaded_images/"):
         for file in files:
-            bits = subdir.split("/")
-            category = bits[-1]
-            print(category)
-            filepath = os.path.join(subdir, file)
-            image = cv2.imread(filepath)
-            image = resize(image, size=256)
-            X.append(image)
-            y.append(category)
+            if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
+                bits = subdir.split("/")
+                category = bits[-1]
+                print(category)
+                filepath = os.path.join(subdir, file)
+                image = cv2.imread(filepath)
+                if image is not None:
+                    image = resize(image, size=size)
+                    X.append(image)
+                    y.append(category)
 
     return X, y
 
+
 def preprocess_data(X, y):
+    '''Preprocesses the data that have already been extracted with extract_data() method
+       Attributes:
+           X: A list containing images of shape (3, width, height)
+           y: A list containing the image labels in string form
+        Returns data in numpy form ready to be used for training
+    '''
 
     X = np.array(X)
     categories = set()
 
-    for label in y:
+    for label in y:  # get all the unique categories
         categories.add(label)
 
-    categories = dict(zip(categories, range(len(categories))))
+    categories = dict(zip(categories, range(len(categories))))  # build a dictionary that maps categories in string form
+                                                                # to a unique id
     y_temp = []
-    for label in y:
+    for label in y:  # encode y with the unique ids
         y_temp.append(categories[label])
 
     y_temp = np.array(y_temp)
     X = X.astype(np.float32)
-    mean = X.mean()
+    mean = X.mean()  # get mean
 
-    fo=open("mean.txt", 'wb+')
-    output="mean,"+mean+"\n"
+    # save mean
+    fo = open("mean.txt", 'wb+')
+    output = "mean,"+str(mean)+"\n"
     fo.write(output)
     fo.close()
 
+    # save categories for future use
     pickle.dump(categories, open("categories.p", "wb"))
 
-
-    X = (X-mean)/255
-    np_utils.to_categorical(y, 1000)
+    X = (X-mean) / 255  # Scale by 255 to get the inputs in the range 0 - 1 so that the CNN can understand them
+    np_utils.to_categorical(y_temp, 1000)
+    print(X.shape)
+    print(y_temp.shape)
     return X, y_temp
 
+
 def get_metadata():
+    '''Load metadata'''
 
     fo = open("mean.txt", 'r')
     lines = fo.readlines()
@@ -62,17 +85,23 @@ def get_metadata():
     mean = float(bits[-1])
     categories = pickle.load(open("categories.p", "rb"))
 
-    return mean
+    return mean, categories
+
 
 def resize(img, size):
-       """resize"""
+    """resize image into size x size
+        Attributes:
+            img: The image to resize
+            size: The size to resize the image
+    """
 
-       img=cv2.resize(img, (size, size))
-
-       return img
+    img = cv2.resize(img, (size, size))
+    img = np.rollaxis(img, 2)
+    return img
 
 
 def split_data(X, y, split_ratio=0.1):
+    ''''''
     random.seed(4096)
     train_idx = []
     test_idx = []
--- deploy.py
+++ deploy.py
@@ -3,36 +3,44 @@ import model as m
 import os
 import data
 import cv2
+import numpy as np
+
 
 def load_model():
+    '''Loads and compiles pre-trained model to be used for real-time predictions'''
     model = m.get_model()
     model.load_weights("model_weights.hdf5")
     model.compile(loss='crossentropy_classification_error', optimizer='adam')
     return model
 
 def get_data_from_folder(test_image_folder):
-    mean = data.get_metadata()
+    '''Extracts images from image folder and gets them ready for use with the deep neural network'''
+    mean, categories = data.get_metadata()
     dir = os.path.dirname(os.path.abspath(__file__))
     images = []
     for subdir, dir, files in os.walk(dir+"/"+test_image_folder+"/"):
         for file in files:
-            filepath=os.path.join(subdir, file)
+            filepath = os.path.join(subdir, file)
             image = cv2.imread(filepath)
+            image = data.resize(image)
+            image = image.astype(np.float32)
             image = (image - mean) / 255
             images.append(image)
 
 def get_data_from_file(filepath):
-    mean = data.get_metadata()
+    '''Get image from file ready to be used with the deep neural network'''
+    mean, categories = data.get_metadata()
     image = cv2.imread(filepath)
     image = (image - mean) / 255
 
     return image
 
 def apply_model(X, model, categories):
+    '''Apply model and produce top 3 predictions for given images'''
     model = Sequential()
     y = []
     for image in X:
-        y_temp = model.predict_proba(X=X, batch_size=128)
+        y_temp = model.predict_proba(X=image, batch_size=1)
         top_n = y_temp.argsort()[-3:][::-1]
         res = []
         for item in top_n:
@@ -41,6 +49,7 @@ def apply_model(X, model, categories):
     return y
 
 if __name__ == '__main__':
+    #If used as script then run example use case
     test_image_path = ''
     test_image_folder = ''
 
