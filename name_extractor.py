dee1f1a61fac4804d5fee5c405be7a21d173c99e
Marica Odagaki <ento.entotto@gmail.com>
2016-03-20 12:56:05 -0700

    Implement list_characters

--- name_extractor.py
+++ name_extractor.py
@@ -10,6 +10,7 @@ import requests
 
 
 ANN_REPORTS_URL = 'http://www.animenewsnetwork.com/encyclopedia/reports.xml'
+ANN_DETAILS_URL = 'http://cdn.animenewsnetwork.com/encyclopedia/api.xml'
 ANN_ANIME_RATINGS_REPORT_ID = 172
 TRAILING_KIND_RE = re.compile(r'\s+\([^)]+\)$')
 
@@ -21,8 +22,8 @@ def get_top_n_shows(n):
     response = requests.get(ANN_REPORTS_URL, params=params)
     root = ElementTree.fromstring(response.content)
     return {
-        'fields': ['show'],
-        'items': map(lambda item: [_extract_item_name(item)], root.iter('item'))
+        'fields': ['id', 'name'],
+        'items': map(lambda item: {'id': item.get('id'), 'name': _extract_item_name(item)}, root.iter('item'))
     }
 
 
@@ -31,14 +32,34 @@ def _extract_item_name(item):
 
 
 def list_characters(shows):
-    pass
+    ids = [show['id'] for show in shows['items']]
+    params = {'anime': ids}
+    response = requests.get(ANN_DETAILS_URL, params=params)
+    root = ElementTree.fromstring(response.content)
+    return {
+        'fields': ['show_id', 'show_name', 'character_name'],
+        'items': _extract_anime_characters(root),
+    }
 
 
-def print_csv(field_items):
-    writer = csv.writer(codecs.getwriter('utf8')(sys.stdout))
-    writer.writerow(field_items['fields'])
+def _extract_anime_characters(root):
+    for anime in root.iter('anime'):
+        anime_id = anime.get('id')
+        anime_name = anime.get('name')
+        for role in anime.findall("cast[@lang='JA']/role"):
+            yield {
+                'anime_id': anime_id,
+                'anime_name': anime_name,
+                'name': role.text,
+            }
+
+
+def print_csv(field_items, f=sys.stdout, only_fields=None):
+    writer = csv.writer(codecs.getwriter('utf8')(f))
+    fields = field_items['fields'] if only_fields is None else only_fields
+    writer.writerow(fields)
     for row in field_items['items']:
-        writer.writerow(row)
+        writer.writerow([row[field] for field in fields])
 
 
 if __name__ == "__main__":
@@ -49,7 +70,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.what == 'shows':
-        print_csv(get_top_n_shows(args.n))
+        print_csv(get_top_n_shows(args.n), ['name'])
     elif args.what == 'characters':
         shows = get_top_n_shows(args.n)
-        print_csv(list_characters(shows))
+        print_csv(list_characters(shows), ['anime_name', 'name'])
--- tests/test_name_extractor.py
+++ tests/test_name_extractor.py
@@ -6,12 +6,31 @@ import name_extractor
 
 def test_top_n_shows(monkeypatch):
     for report, expected in [
-            (two_shows, [['Steins;Gate'], ['Fullmetal Alchemist: Brotherhood']]),
+            (two_shows, [
+                {'id': '11770', 'name': 'Steins;Gate'},
+                {'id': '10216', 'name': 'Fullmetal Alchemist: Brotherhood'}]),
             (no_shows, []),
     ]:
         monkeypatch.setattr(requests, 'get', mock_get(report))
         shows = name_extractor.get_top_n_shows(100)
         assert shows['items'] == expected
+
+
+def test_list_characters(monkeypatch):
+    for xml, expected in [
+            (two_details, [
+                {'anime_id': '11770', 'anime_name': 'Steins;Gate', 'name': 'Mayuri Shiina'},
+                {'anime_id': '9701', 'anime_name': 'Clannad After Story', 'name': 'Fuuko Ibuki'},
+            ]),
+            (no_details, [])
+    ]:
+        monkeypatch.setattr(requests, 'get', mock_get(xml))
+        shows = {
+            'fields': ['show'],
+            'items': [{'id': '11770'}, {'id': '10216'}],
+        }
+        characters = name_extractor.list_characters(shows)['items']
+        assert list(characters) == expected
 
 
 def mock_get(content):
@@ -37,4 +56,32 @@ two_shows = '''
 no_shows = '''
 <report>
 </report>
+'''
+
+two_details = '''
+<ann>
+  <anime id="11770" name="Steins;Gate">
+    <cast gid="3133619099" lang="DE">
+      <role>Kurisu Makise</role>
+      <person id="105651">Manuela Bäcker</person>
+    </cast>
+    <cast gid="3936561558" lang="JA">
+      <role>Mayuri Shiina</role>
+      <person id="53741">Kana Hanazawa</person>
+    </cast>
+  </anime>
+  <anime id="10216" name="Fullmetal Alchemist: Brotherhood">
+  </anime>
+  <anime id="9701" gid="4003373163" type="TV" name="Clannad After Story">
+    <cast gid="2029100002" lang="JA">
+      <role>Fuuko Ibuki</role>
+      <person id="13321">Ai Nonaka</person>
+    </cast>
+  </anime>
+</ann>
+'''
+
+no_details = '''
+<ann>
+</ann>
 '''
